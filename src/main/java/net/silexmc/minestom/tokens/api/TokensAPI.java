f8bc2327c676585c295a616559d5865110fa49e1
ChrisGenti <gentile.christian@outlook.it>
2025-01-15 15:44:27 +0100

    Initial Commit

---
+++ src/main/java/net/silexmc/minestom/tokens/api/TokensAPI.java
@@ -0,0 +1,38 @@
+package net.silexmc.minestom.tokens.api;
+
+import java.math.BigInteger;
+import java.util.UUID;
+
+public interface TokensAPI {
+    BigInteger getTokens(UUID uniqueID);
+
+    String getTokens(UUID uniqueID, String format);
+
+    BigInteger getTotalTokens(UUID uniqueID);
+
+    String getTotalTokens(UUID uniqueID, String format);
+
+    void setTokens(UUID uniqueID, BigInteger amount);
+
+    default void setTokens(UUID uniqueID, int amount) {
+        this.setTokens(uniqueID, BigInteger.valueOf(amount));
+    }
+
+    void giveTokens(UUID uniqueID, BigInteger amount);
+
+    default void giveTokens(UUID uniqueID, int amount) {
+        this.giveTokens(uniqueID, BigInteger.valueOf(amount));
+    }
+
+    void takeTokens(UUID uniqueID, BigInteger amount);
+
+    default void takeTokens(UUID uniqueID, int amount) {
+        this.takeTokens(uniqueID, BigInteger.valueOf(amount));
+    }
+
+    boolean hasTokens(UUID uniqueID, BigInteger amount);
+
+    default boolean hasTokens(UUID uniqueID, int amount) {
+        return this.hasTokens(uniqueID, BigInteger.valueOf(amount));
+    }
+}
